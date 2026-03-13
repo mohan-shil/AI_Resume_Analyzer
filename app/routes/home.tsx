8d61e4d6014e5ab8c2a305da7d4e8297da064919
mohan-shil <mohan.shil.007@gmail.com>
2026-03-13 22:08:16 +0100

    Added CV images but not showing it properly

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
-import  ResumeCard  from "~/components/resumeCard";
+import  ResumeCard  from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,25 +12,22 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className= "bg-[url('/images/bg-main.svg')] bg-cover" >
-    <Navbar />
+  <Navbar />
 
 
-    <section className="main-section">
-      <div className="page-heading">
+  <section className="main-section">
+      <div className="page-heading py-16">
         <h1> Track Your Application & Resume Ratings </h1>
         <h2> Review your submission and check AI powered feedback </h2>
       </div>
+   
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map(resume => (
+            <ResumeCard key={resume.id} resume={resume} /> 
+          ))}
+        </div>
+      )} 
     </section>
-
-{resumes.length > 0 && (
-
-<div className="resumes-section">
-  {resumes.map((resume) => (
-    <ResumeCard key={resume.id} resume={resume} /> 
-  ))}
-
-</div>
-)}
-
   </main>
 }
